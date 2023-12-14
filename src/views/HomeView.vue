43d82d6fe29dbcbf58272e1d088d6ac36e97fd33
StrictKraken <StefanStark77@gmail.com>
2023-12-14 11:34:07 +0300

    refactor: auto format

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent } from "vue";
 // import AutoComplete from "@/components/AddUsers/AutoComplete.vue";
 // import FriendView from "@/components/Friends/FriendsView.vue";
 import AuthWrapper from "@/components/AuthComonent/AuthWrapper.vue";
-const AutoComplete = defineAsyncComponent(() => import("@/components/AddUsers/AutoComplete.vue"))
-const FriendView = defineAsyncComponent(() => import("@/components/Friends/FriendsView.vue"))
+const AutoComplete = defineAsyncComponent(() => import("@/components/AddUsers/AutoComplete.vue"));
+const FriendView = defineAsyncComponent(() => import("@/components/Friends/FriendsView.vue"));
 </script>
 
 <template>
